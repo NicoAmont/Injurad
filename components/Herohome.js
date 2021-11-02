2910c19f4b572df1a95a0af12b2280b44cac1a33
NicoAmont <nicolas.amezquita.m@gmail.com>
2021-11-02 02:12:53 -0500

    a

--- components/Herohome.js
+++ components/Herohome.js
@@ -6,8 +6,8 @@ export default function Herohome() {
                 <path d="M648.752 350.865C343.491 311.51 95.6904 480.464 0 453.486L6.39515 828H2050V46.1763C1994.81 18.4933 1855.02 -25.6584 1737.35 19.1986C1590.26 75.2698 1583.86 124.464 1433.22 258.824C1282.58 393.183 1030.33 400.06 648.752 350.865Z" fill="url(#paint0_linear_4:674)"/>
                 <defs>
                 <linearGradient id="paint0_linear_4:674" x1="1025" y1="0" x2="1025" y2="828" gradientUnits="userSpaceOnUse">
-                <stop stop-color="#F8FAFF"/>
-                <stop offset="1" stop-color="white" stop-opacity="0"/>
+                <stop stopColor="#F8FAFF"/>
+                <stop offset="1" stopColor="white" stopOpacity="0"/>
                 </linearGradient>
                 </defs>
             </svg>
@@ -67,7 +67,7 @@ export default function Herohome() {
                         <rect x="26.3994" y="14.6934" width="4.26117" height="15.3696" transform="rotate(89.8332 26.3994 14.6934)" fill="#DC0000"/>
                         <rect x="21.5625" y="14.375" width="5.5147" height="11.0294" fill="#DC0000"/>
                         <path d="M18.7499 9.55908L26.4705 14.7061H11.0293L18.7499 9.55908Z" fill="#DC0000"/>
-                        <circle cx="18.75" cy="18.75" r="17.25" stroke="white" stroke-width="3"/>
+                        <circle cx="18.75" cy="18.75" r="17.25" stroke="white" strokeWidth="3"/>
                         <rect x="26.4707" y="35.2178" width="1.47059" height="6.61764" transform="rotate(-26.6177 26.4707 35.2178)" fill="white"/>
                         <rect x="27.6533" y="41.8525" width="5.66063" height="18.2638" transform="rotate(-26.6177 27.6533 41.8525)" fill="white"/>
                         <path d="M28.944 39.6729L29.74 41.0515L27.6572 41.8787L28.944 39.6729Z" fill="white"/>
@@ -90,7 +90,7 @@ export default function Herohome() {
             </div>
             <div className="flex flex-col items-center justify-center">
                     <h1 className=" ml-4 mt-40 mb-7 text-bd">Únete a esta gran comunidad , has parte de BUSKAMOS.</h1>
-                    <button class="bg-rb rounded-full px-8 py-2 shadow-lg uppercase mb-7">registrate</button>
+                    <button className="bg-rb rounded-full px-8 py-2 shadow-lg uppercase mb-7">registrate</button>
             </div>
             
             <div className="flex justify-center">
