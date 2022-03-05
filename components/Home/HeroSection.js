6d9ba5d314ac235fcf661d7ab57f6bfb25427a99
NicoAmont <nicolas.amezquita.m@gmail.com>
2022-03-05 13:56:52 -0500

    finish landing

--- components/Home/HeroSection.js
+++ components/Home/HeroSection.js
@@ -10,8 +10,8 @@ export default function HeroSection() {
         
         <div className="w-full h-screen font-heads bg-bgf  relative flex flex-col ">
             
-                <iframe className="relative mt-28 md:mt-36 z-10 w-screen m-auto h-3/4 scale-110 border-2 border-sl rounded-lg shadow-xl" src={ytvideo[0]} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-                <Image className='absolute   h-full opacity-5 bottom-0 '  src="/heroimg.png" layout='fill' objectFit='cover' objectPosition='center' alt="" />
+                <iframe className="relative mt-28 md:mt-36 z-10 w-screen m-auto h-3/4 scale-110  rounded-lg shadow-xl" src={ytvideo[0]} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                <Image className='absolute h-full opacity-5 bottom-0 '  src="/heroimg.png" layout='fill' objectFit='cover' objectPosition='center' alt="" />
                 
             
         </div>
--- components/Home/Slogan.js
+++ components/Home/Slogan.js
@@ -6,6 +6,7 @@ function Slogan() {
     return (
         <div className="w-screen h-full py-10 sm:py-10 flex justify-center font-heads font-medium m-auto bg-bgrS">
             <h1 className=" max-w-6xl m-auto text-xl px-6 "><span className="uppercase text-2xl text-sl font-semibold">{t('slogan2')}</span> {t('slogan')}</h1>
+            <h1 className=" max-w-6xl m-auto text-xl px-6 ">{t('slogan')}</h1>
         </div>
     )
 }
