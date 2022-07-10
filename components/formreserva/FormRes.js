66a2e21578ad73e2ffca9db41c5a76103a30021b
NicoAmont <nicolas.amezquita.m@gmail.com>
2022-07-10 01:22:18 -0500

    adding texts

--- components/formreserva/FormRes.js
+++ components/formreserva/FormRes.js
@@ -372,7 +372,7 @@ export default function FormRes() {
                     htmlFor="valor_inm" 
                     className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transhtmlForm -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
                 >
-                    Valor del inmueble (ej : 250.000)
+                    Valor del inmueble (ej : 250000)
                 </label>
             </div>
             <div className="relative z-0 mb-6 w-full group">
