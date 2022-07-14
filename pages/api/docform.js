f0a67cdded61bfa70398a76247b4add4d836059b
NicoAmont <nicolas.amezquita.m@gmail.com>
2022-07-14 16:51:47 -0500

    adding styles

--- pages/api/docform.js
+++ pages/api/docform.js
@@ -226,11 +226,11 @@ export default async function handler(req, res) {
         </head>
         <body>
           <div class="container">
-          <div class="container-text " >
+          <div class="container_text " >
           <img src="https://res.cloudinary.com/lewt-copr/image/upload/v1656302423/injurad_sflh0t.png" alt="">
           <div class="container_subtext">
 
-            <h1 class="container_subtext " >
+            <h1 class="titles " >
               ${text.text1}
             </h1>
   
